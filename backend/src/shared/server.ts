995a18683fb3492f5a84134b8d98f6add2349263
guilhermeaw <guilherme.w@universo.univates.br>
2022-08-25 18:27:13 -0300

    Trigger pipeline

--- backend/src/shared/server.ts
+++ backend/src/shared/server.ts
@@ -3,5 +3,5 @@ import app from './app';
 const serverPort = 3333;
 
 app.listen(serverPort, () => {
-  console.log(`Server up on port ${serverPort}!`);
+  console.log(`Server up on port ${serverPort}`);
 });

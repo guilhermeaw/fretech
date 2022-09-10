795aafbc6db6f3eb2739ff4b446f732f3bd97bdb
guilhermeaw <guilherme.w@universo.univates.br>
2022-09-10 20:14:32 -0300

    Prepara rotas para trabalhar com autenticação

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,14 +2,25 @@ import { BrowserRouter, Route, Routes as DOMRoutes } from 'react-router-dom';
 import { Header } from '../components/Header';
 import Login from '../pages/Login';
 
+const AppRoutes = () => (
+  <>
+    <Header />
+    <DOMRoutes>
+      <Route path="/encomendas" element={<h1>encomendas</h1>} />
+    </DOMRoutes>
+  </>
+);
+
+const AuthRoutes = () => (
+  <DOMRoutes>
+    <Route path="/" element={<Login />} />
+  </DOMRoutes>
+);
+
 export const Routes = () => {
+  const logged = true;
+
   return (
-    <BrowserRouter>
-      {/* <Header /> */}
-      <DOMRoutes>
-        <Route path="/" element={<Login />} />
-        <Route path="/encomendas" element={<h1>encomendas</h1>} />
-      </DOMRoutes>
-    </BrowserRouter>
+    <BrowserRouter>{logged ? <AppRoutes /> : <AuthRoutes />}</BrowserRouter>
   );
 };

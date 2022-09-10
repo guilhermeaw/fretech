56aeb845400dbacaf13ead8bca58794957ebfea5
guilhermeaw <guilherme.w@universo.univates.br>
2022-09-09 22:51:51 -0300

    Adiciona config para typeORM

--- backend/src/shared/app.ts
+++ backend/src/shared/app.ts
@@ -1,14 +1,18 @@
 import express, { Request, Response } from 'express';
 
+import { initializeDataSource } from './database/ormconfig';
+
+initializeDataSource();
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (request: Request, response: Response) => {
+app.get('/', (_: Request, response: Response) => {
   return response.json({ message: 'API working as expected!' });
 });
 
-app.get('/teste', (request: Request, response: Response) => {
+app.get('/teste', (_: Request, response: Response) => {
   return response.json({ message: 'Teste' });
 });
 
---
+++ backend/src/shared/database/ormconfig.ts
@@ -0,0 +1,30 @@
+import { DataSource } from 'typeorm';
+
+const entitiesDir =
+  process.env.NODE_ENV === 'dev'
+    ? 'src/modules/**/entities/*.ts'
+    : 'dist/modules/**/entities/*.js';
+
+const AppDataSource = new DataSource({
+  name: 'default',
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'postgres',
+  database: 'fretech',
+  entities: [entitiesDir],
+  synchronize: true,
+});
+
+export const initializeDataSource = () => {
+  AppDataSource.initialize()
+    .then(() => {
+      console.log('Data Source has been initialized!');
+    })
+    .catch(err => {
+      console.error('Error during Data Source initialization:', err);
+    });
+};
+
+export default AppDataSource;

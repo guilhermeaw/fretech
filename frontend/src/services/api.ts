fdcf93518c44a2894f8605049bd57eddd37a5760
guilhermeaw <guilherme.w@universo.univates.br>
2022-09-11 21:17:02 -0300

    Adiciona interceptor para o axios

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -3,3 +3,13 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
+
+api.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    console.error(error);
+    return Promise.reject(error.response.data);
+  },
+);

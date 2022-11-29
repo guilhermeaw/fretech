2dc5fdcc1a5890e21019795948a4df260b16da2c
GUILHERME AUGUSTO WEISSHEIMER <guilherme.w@universo.univates.br>
2022-11-29 22:33:33 +0000

    Adiciona configuração de testes para o front

---
+++ frontend/src/components/EmptyList/EmptyList.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+
+import { EmptyList } from '.';
+
+describe('EmptyList', () => {
+    it('should render an image, a heading and a message passed by prop', () => {
+        const message = 'Sem ocorrências cadastradas';
+        
+        render(<EmptyList message={message} />);
+
+        expect(screen.getByAltText(/caixa vazia/i));
+        expect(screen.getByRole('heading', { name: /nada por aqui/i }));
+        expect(screen.getByText(message));
+    })
+})
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -21,5 +22,10 @@ export default defineConfig(({ mode }) => {
     server: {
       port: 8080,
     },
+    test: {
+      setupFiles: 'src/setupTests.ts',
+      globals: true,
+      environment: 'jsdom'
+    }
   };
 });
